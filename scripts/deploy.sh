323ffe2969dcaa0cf3be85a427a4af19c91adfb7
HunSeongPark <phunseong@gmail.com>
2022-12-29 19:15:13 +0900

    app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ sudo docker rmi gnstjd0831/sample:latest
 sudo docker pull gnstjd0831/sample:latest
 
 # 도커 run
-docker run -d -p 8080:8080 --name sample gnstjd0831/sample:latest
+docker run -d -p 8080:8080 -v /home/ec2-user:/config --name sample gnstjd0831/sample:latest
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
