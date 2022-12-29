0aa2926a63c906e08a0b2bc2e2373c891627091e
HunSeongPark <phunseong@gmail.com>
2022-12-29 19:01:02 +0900

    application.properties test

--- src/main/java/com/example/depexample/controller/IndexController.java
+++ src/main/java/com/example/depexample/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.depexample.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    @Value("${hunseong.name}")
+    private String name;
+
     @GetMapping("/")
     public String index() {
-        return "ver_3";
+        return name;
     }
 }
